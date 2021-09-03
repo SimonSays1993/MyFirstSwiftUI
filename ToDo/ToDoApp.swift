d9d7efc52294cb2160405ff21b752e33fbc6ceac
Simon McNeil <simonmcneil062@gmail.com>
2021-09-03 08:41:53 -0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Simon Mcneil on 2021-09-03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
